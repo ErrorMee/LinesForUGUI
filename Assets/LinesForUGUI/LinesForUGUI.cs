ee55e02dbc74d59cebb8910620f6173d2d5ae661
chuy <chuy@xiaopeng.com>
2023-05-31 15:29:31 +0800

    opt

--- Assets/LinesForUGUI/LinesForUGUI.cs
+++ Assets/LinesForUGUI/LinesForUGUI.cs
@@ -11,8 +11,7 @@ public class LinesForUGUI : Image
     [SerializeField] List<LineInfo> lines;
 
     int vertexCount = 0; LineInfo lineCrt;
-    UIVertex vertexLastLeft; UIVertex vertexLastRight;
-    Vector3 lastCtrPos;
+    Vector3 lastCtrPos; float lineDis = 0;
 
     public void Draw(List<LineInfo> lines)
     {
@@ -29,98 +28,14 @@ public class LinesForUGUI : Image
         }
     }
 
-    private Vector3 PointDir(Vector3 fromPos, Vector3 toPos)
-    {
-        Vector3 pointDir = (fromPos - toPos).normalized;
-        if (pointDir == Vector3.zero)
-        {
-            pointDir = Vector3.right;
-        }
-        return pointDir;
-    }
-
-    private void AddTwoHeadVert(VertexHelper toFill, PointInfo ctrPoint)
-    {
-        Vector3 pointDir = Vector3.left;
-        if (lineCrt.points.Count > 1)
-        {
-            pointDir = PointDir(ctrPoint.pos, lineCrt.points[1].pos);
-        }
-        Vector3 wideDir = new(-pointDir.y, pointDir.x, 0);
-        Vector3 wideOffset = wideDir * (ctrPoint.radius + lineCrt.roundRadius);
-        Vector3 posOffset = lineCrt.roundRadius * pointDir;
-
-        UIVertex vertexLeft = UIVertex.simpleVert;
-        vertexLeft.position = ctrPoint.pos + posOffset - wideOffset;
-        vertexLeft.color = ctrPoint.color * color;
-        // ctrPos fadeRadius
-        vertexLeft.uv0 = new Vector4(ctrPoint.pos.x, ctrPoint.pos.y, lineCrt.fadeRadius);
-        // sdOrientedBox: thickness, round, blank
-        vertexLeft.uv1 = new Vector4(ctrPoint.radius * 2, lineCrt.roundRadius, lineCrt.blankStart, lineCrt.blankLen);
-        // os
-        vertexLeft.uv2 = new Vector4(vertexLeft.position.x, vertexLeft.position.y);
-        toFill.AddVert(vertexLeft);
-        Debug.LogError("vertexLeft " + vertexLeft.position);
-        vertexLastLeft = vertexLeft;
-
-        UIVertex vertexRight = vertexLeft;
-        vertexRight.position = ctrPoint.pos + posOffset + wideOffset;
-        vertexRight.uv2 = new Vector4(vertexRight.position.x, vertexRight.position.y);
-        toFill.AddVert(vertexRight);
-        Debug.LogError("vertexRight " + vertexRight.position);
-        vertexLastRight = vertexRight;
-
-        lastCtrPos = (vertexLastLeft.position + vertexLastRight.position) * 0.5f;
-        vertexCount += 2;
-    }
-
-    private void AddMidRect(VertexHelper toFill, PointInfo ctrPoint, Vector3 nexCtrPos)
-    {
-        Vector3 dirPreToCrt = PointDir(ctrPoint.pos, lastCtrPos);
-        Vector3 dirCrtToNex = PointDir(nexCtrPos, ctrPoint.pos);
-        Vector3 dirAverage = (dirPreToCrt + dirCrtToNex) * 0.5f;
-
-        Vector3 wideDir = new(-dirAverage.y, dirAverage.x, 0);
-
-        float cos = dirAverage.x * dirPreToCrt.x + dirAverage.y * dirPreToCrt.y;
-        float dCos = Mathf.Min(1.0f / cos, 99999);
-        Vector3 wideOffset = dCos * (ctrPoint.radius + lineCrt.roundRadius) * wideDir;
-
-        UIVertex vertexLeft = UIVertex.simpleVert;
-        vertexLeft.position = ctrPoint.pos + wideOffset;
-        vertexLeft.color = ctrPoint.color * color;
-        // ctrPos fadeRadius
-        vertexLeft.uv0 = new Vector4(ctrPoint.pos.x, ctrPoint.pos.y, lineCrt.fadeRadius);
-        // sdOrientedBox: thickness, round, blank
-        vertexLeft.uv1 = new Vector4(ctrPoint.radius * 2, lineCrt.roundRadius, lineCrt.blankStart, lineCrt.blankLen);
-        // os
-        vertexLeft.uv2 = new Vector4(vertexLeft.position.x, vertexLeft.position.y);
-        toFill.AddVert(vertexLeft);
-        Debug.LogError("vertexLeft " + vertexLeft.position);
-        vertexLastLeft = vertexLeft;
-
-        UIVertex vertexRight = vertexLeft;
-        vertexRight.position = ctrPoint.pos - wideOffset;
-        vertexRight.uv2 = new Vector4(vertexRight.position.x, vertexRight.position.y);
-        toFill.AddVert(vertexRight);
-        Debug.LogError("vertexRight " + vertexRight.position);
-        vertexLastRight = vertexRight;
-
-        lastCtrPos = ctrPoint.pos;
-        vertexCount += 2;
-
-        AddTwoTriangle(toFill);
-    }
-
     private void Draw(VertexHelper toFill, LineInfo lineInfo)
     {
         if (lineInfo.points.Count < 1)
         {
             return;
         }
-        lineCrt = lineInfo; 
-
-        AddTwoHeadVert(toFill, lineCrt.points[0]);
+        lineCrt = lineInfo; lineDis = 0;
+        AddStartVert(toFill);
 
         for (int i = 0; i < lineInfo.points.Count - 1; i++)
         {
@@ -129,10 +44,109 @@ public class LinesForUGUI : Image
         if (lineInfo.points.Count > 1)
         {
             PointInfo ctrPoint = lineInfo.points[^1];
-            Vector3 dirPreToCrt = PointDir(ctrPoint.pos, lineInfo.points[^2].pos);
+            Vector3 dirPreToCrt = PointDir(lineInfo.points[^2].pos, ctrPoint.pos);
             AddMidRect(toFill, ctrPoint, ctrPoint.pos + dirPreToCrt);
         }
-        
+        AddEndRect(toFill);
+    }
+
+    private Vector3 PointDir(Vector3 fromPos, Vector3 toPos)
+    {
+        Vector3 pointDir = (toPos - fromPos).normalized;
+        if (pointDir == Vector3.zero)
+        {
+            pointDir = Vector3.right;
+        }
+        return pointDir;
+    }
+
+    private void AddStartVert(VertexHelper toFill)
+    {
+        PointInfo ctrPoint = lineCrt.points[0];
+        Vector3 pointDir = Vector3.left;
+        if (lineCrt.points.Count > 1)
+        {
+            pointDir = PointDir(lineCrt.points[1].pos, ctrPoint.pos);
+        }
+        Vector3 wideDir = new(-pointDir.y, pointDir.x, 0);
+        AddTwoVert(toFill, ctrPoint, pointDir, wideDir);
+    }
+
+    private void AddEndRect(VertexHelper toFill)
+    {
+        PointInfo ctrPoint = lineCrt.points[^1];
+        Vector3 pointDir = Vector3.right;
+        if (lineCrt.points.Count > 1)
+        {
+            pointDir = PointDir(lineCrt.points[^2].pos, ctrPoint.pos);
+        }
+        Vector3 wideDir = new(-pointDir.y, pointDir.x, 0);
+        AddTwoVert(toFill, ctrPoint, pointDir, -wideDir);
+        AddTwoTriangle(toFill);
+    }
+
+    private void AddTwoVert(VertexHelper toFill, PointInfo ctrPoint, Vector3 pointDir, Vector3 wideDir)
+    {
+        float radius = ctrPoint.radius + lineCrt.radius;
+        Vector3 wideOffset = wideDir * radius;
+        Vector3 posOffset = pointDir * radius;
+
+        UIVertex vertexLeft = UIVertex.simpleVert;
+        vertexLeft.position = ctrPoint.pos + posOffset - wideOffset;
+        vertexLeft.color = ctrPoint.color * color;
+        // ctrPos fadeRadius
+        vertexLeft.uv0 = new Vector4(ctrPoint.pos.x, ctrPoint.pos.y, lineCrt.fadeRadius);
+        // radius, blankStart, blankLen, lineDis
+
+        vertexLeft.uv1 = new Vector4(radius, lineCrt.blankStart, lineCrt.blankLen, lineDis);
+        // os
+        vertexLeft.uv2 = new Vector4(vertexLeft.position.x, vertexLeft.position.y);
+        toFill.AddVert(vertexLeft);
+
+        UIVertex vertexRight = vertexLeft;
+        vertexRight.position = ctrPoint.pos + posOffset + wideOffset;
+        vertexRight.uv2 = new Vector4(vertexRight.position.x, vertexRight.position.y);
+        toFill.AddVert(vertexRight);
+
+        lastCtrPos = (vertexLeft.position + vertexRight.position) * 0.5f;
+        vertexCount += 2;
+    }
+
+    private void AddMidRect(VertexHelper toFill, PointInfo ctrPoint, Vector3 nexCtrPos)
+    {
+        Vector3 dirPreToCrt = PointDir(lastCtrPos, ctrPoint.pos);
+        Vector3 dirCrtToNex = PointDir(ctrPoint.pos, nexCtrPos);
+        Vector3 dirAverage = (dirPreToCrt + dirCrtToNex) * 0.5f;
+
+        Vector3 wideDir = new(-dirAverage.y, dirAverage.x, 0);
+
+        float cos = dirAverage.x * dirPreToCrt.x + dirAverage.y * dirPreToCrt.y;
+        float zoom = Mathf.Min(1.0f / cos, 99999);
+        float radius = lineCrt.radius + ctrPoint.radius;
+        Vector3 wideOffset = zoom * radius * wideDir;
+
+        UIVertex vertexLeft = UIVertex.simpleVert;
+        vertexLeft.position = ctrPoint.pos + wideOffset;
+        vertexLeft.color = ctrPoint.color * color;
+        // ctrPos fadeRadius
+        vertexLeft.uv0 = new Vector4(ctrPoint.pos.x, ctrPoint.pos.y, lineCrt.fadeRadius);
+        // radius, blankStart, blankLen, lineDis
+        lineDis += (lastCtrPos - ctrPoint.pos).magnitude;
+        vertexLeft.uv1 = new Vector4(Mathf.Sqrt(zoom) * radius, lineCrt.blankStart, lineCrt.blankLen, lineDis);
+        // os
+        vertexLeft.uv2 = new Vector4(vertexLeft.position.x, vertexLeft.position.y);
+        toFill.AddVert(vertexLeft);
+        //Debug.LogError("vertexLeft " + vertexLeft.position + " lineDis " + lineDis);
+
+        UIVertex vertexRight = vertexLeft;
+        vertexRight.position = ctrPoint.pos - wideOffset;
+        vertexRight.uv2 = new Vector4(vertexRight.position.x, vertexRight.position.y);
+        toFill.AddVert(vertexRight);
+
+        lastCtrPos = ctrPoint.pos;
+        vertexCount += 2;
+
+        AddTwoTriangle(toFill);
     }
 
     private void AddTwoTriangle(VertexHelper toFill)
--- Assets/LinesForUGUI/LinesForUGUI.shader
+++ Assets/LinesForUGUI/LinesForUGUI.shader
@@ -17,136 +17,116 @@ Shader "LinesForUGUI"
         [Toggle(UNITY_UI_ALPHACLIP)] _UseUIAlphaClip("Use Alpha Clip", Float) = 0
     }
 
-        SubShader
+    SubShader
+    {
+        Tags
         {
-            Tags
-            {
-                "Queue" = "Transparent"
-                "IgnoreProjector" = "True"
-                "RenderType" = "Transparent"
-                "PreviewType" = "Plane"
-                "CanUseSpriteAtlas" = "True"
-            }
-
-            Stencil
-            {
-                Ref[_Stencil]
-                Comp[_StencilComp]
-                Pass[_StencilOp]
-                ReadMask[_StencilReadMask]
-                WriteMask[_StencilWriteMask]
-            }
-
-            Cull Off
-            Lighting Off
-            ZWrite Off
-            ZTest[unity_GUIZTestMode]
-            Blend SrcAlpha OneMinusSrcAlpha
-            ColorMask[_ColorMask]
-
-            Pass
-            {
-                Name "Default"
-            CGPROGRAM
-                #pragma vertex vert
-                #pragma fragment frag
-                #pragma target 2.0
-
-                #include "UnityCG.cginc"
-                #include "UnityUI.cginc"
-
-                #pragma multi_compile_local _ UNITY_UI_CLIP_RECT
-                #pragma multi_compile_local _ UNITY_UI_ALPHACLIP
-
-                struct appdata_t
-                {
-                    float4 vertex   : POSITION;
-                    float4 color    : COLOR;
-                    float4 abPos : TEXCOORD0;
-                    float4 custom : TEXCOORD1;//(x:Box thickness) (y:round) (z:blank start) (w:blank len) 
-                    float4 custom1 : TEXCOORD2;//(xy:os) (z:fadeRadius) (w:offsetStart)
-                    UNITY_VERTEX_INPUT_INSTANCE_ID
-                };
-
-                struct v2f
-                {
-                    float4 vertex   : SV_POSITION;
-                    float4 color : COLOR;
-                    float4 abPos  : TEXCOORD0;
-                    float4 worldPosition : TEXCOORD1;
-                    float4 custom : TEXCOORD2;
-                    float4 custom1 : TEXCOORD3;
-                    UNITY_VERTEX_OUTPUT_STEREO
-                };
-
-                sampler2D _MainTex;
-                fixed4 _TextureSampleAdd;
-                float4 _ClipRect;
-                float4 _MainTex_ST;
-
-                v2f vert(appdata_t v)
-                {
-                    v2f OUT;
-                    UNITY_SETUP_INSTANCE_ID(v);
-                    UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(OUT);
-                    OUT.worldPosition = v.vertex;
-                    OUT.vertex = UnityObjectToClipPos(OUT.worldPosition);
-
-                    OUT.abPos = v.abPos;
-                    OUT.color = v.color;
-                    OUT.custom = v.custom;
-                    OUT.custom1 = v.custom1;
-                    return OUT;
-                }
-
-                float sdOrientedBox(in float2 p, in float2 a, in float2 b, float th)
-                {
-                    float l = length(b - a);
-                    float2 d = (b - a) / l;
-                    float2 q = (p - (a + b) * 0.5);
-                    q = mul(float2x2(d.x, -d.y, d.y, d.x), q);
-                    q = abs(q) - float2(l, th) * 0.5;
-                    return length(max(q, 0.0)) + min(max(q.x, q.y), 0.0);
-                }
-
-                half4 frag(v2f IN) : SV_Target
-                {
-                    return 1;
-                    float round = IN.custom.y;
-                    float sdGlobal = sdOrientedBox(IN.custom1.xy, IN.abPos.xw, IN.abPos.zy, IN.custom.x) - round;
-
-
-                    float offsetStart = IN.custom1.w + round;
-                    float2 a2bDir = normalize(IN.abPos.zw - IN.abPos.xy);
-                    float round2 = round * 2;
-                    float solidLen = IN.custom.z; float blankLen = IN.custom.w;
-                    float blockLen = solidLen + blankLen + round2;
-                    int blockIndex = floor(offsetStart / blockLen);
-
-                    float4 abPos;
-                    float2 aOriPos = IN.abPos.xy + a2bDir * blockLen * blockIndex;
-                    abPos.xy = aOriPos;//IN.abPos.xy ??? blockIndex
-                    abPos.zw = aOriPos + a2bDir * solidLen;
-                    float sdLocal = sdOrientedBox(IN.custom1.xy, abPos.xw, abPos.zy, IN.custom.x) - round;
-
-                    sdLocal -= step(IN.custom.w, 0) * 1024;
-                    
-                    float sd = max(sdLocal, sdGlobal);
-                    sd = sdLocal;
-                    half4 color = IN.color;
-                    color.a *= saturate(-sd * (1 / IN.custom1.z));
-
-                    #ifdef UNITY_UI_CLIP_RECT
-                    color.a *= UnityGet2DClipping(IN.worldPosition.xy, _ClipRect);
-                    #endif
-
-                    #ifdef UNITY_UI_ALPHACLIP
-                    clip(color.a - 0.001);
-                    #endif
-                    
-                    return color;
-                }
-            ENDCG
-            }
+            "Queue" = "Transparent"
+            "IgnoreProjector" = "True"
+            "RenderType" = "Transparent"
+            "PreviewType" = "Plane"
+            "CanUseSpriteAtlas" = "True"
         }
+
+        Stencil
+        {
+            Ref[_Stencil]
+            Comp[_StencilComp]
+            Pass[_StencilOp]
+            ReadMask[_StencilReadMask]
+            WriteMask[_StencilWriteMask]
+        }
+
+        Cull Off
+        Lighting Off
+        ZWrite Off
+        ZTest[unity_GUIZTestMode]
+        Blend SrcAlpha OneMinusSrcAlpha
+        ColorMask[_ColorMask]
+
+        Pass
+        {
+            Name "Default"
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 2.0
+
+            #include "UnityCG.cginc"
+            #include "UnityUI.cginc"
+
+            #pragma multi_compile_local _ UNITY_UI_CLIP_RECT
+            #pragma multi_compile_local _ UNITY_UI_ALPHACLIP
+
+            struct appdata_t
+            {
+                float4 vertex   : POSITION;
+                float4 color    : COLOR;
+                float4 custom0 : TEXCOORD0;//ctrPos(xy) fadeRadius(z)
+                float4 custom1 : TEXCOORD1;//radius, blankStart, blankLen, lineDis
+                float4 custom2 : TEXCOORD2;//os(xy)
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+
+            struct v2f
+            {
+                float4 vertex   : SV_POSITION;
+                float4 color : COLOR;
+                float4 worldPosition : TEXCOORD0;
+                float4 custom0  : TEXCOORD1;
+                float4 custom1 : TEXCOORD2;
+                float4 custom2 : TEXCOORD3;
+                UNITY_VERTEX_OUTPUT_STEREO
+            };
+
+            sampler2D _MainTex;
+            float4 _ClipRect;
+
+            v2f vert(appdata_t v)
+            {
+                v2f OUT;
+                UNITY_SETUP_INSTANCE_ID(v);
+                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(OUT);
+                OUT.worldPosition = v.vertex;
+                OUT.vertex = UnityObjectToClipPos(OUT.worldPosition);
+                OUT.color = v.color;
+
+                OUT.custom0 = v.custom0;//circleCenter(xy) fadeRadius(z)
+                OUT.custom1 = v.custom1;//radius, blankStart, blankLen, lineDis
+                OUT.custom2 = v.custom2;//os(xy)
+                return OUT;
+            }
+
+            half4 frag(v2f IN) : SV_Target
+            {
+                float2 sdfPos = IN.custom2.xy;
+                float2 circleCenter = IN.custom0.xy;
+
+                float sd = length(sdfPos - circleCenter) - IN.custom1.x;
+
+                /*float blankStart = IN.custom1.y;
+                float blankLen = IN.custom1.z;
+                float lineDis = IN.custom1.w;
+                float cycleLen = fmod(lineDis, blankStart + blankLen);
+                float blank = step(blankStart, cycleLen);
+                sd += blank * 999;*/
+                    
+                half4 color = IN.color;
+                float fade = saturate(-sd * (1 / IN.custom0.z));
+                fade *= fade; fade *= fade;
+                color.a *= fade;
+                   
+                #ifdef UNITY_UI_CLIP_RECT
+                color.a *= UnityGet2DClipping(IN.worldPosition.xy, _ClipRect);
+                #endif
+
+                #ifdef UNITY_UI_ALPHACLIP
+                clip(color.a - 0.001);
+                #endif
+                    
+                return color;
+            }
+            ENDCG
+        }
+    }
 }
